adc86887fbf8d480442f98a6e762dd70a1078f20
Seohee  Im <seoheeim@vt.edu>
2020-08-10 15:18:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  first_card = deal_card
-  second_card = deal_card
+  first_card == deal_card
+  second_card == deal_card
   card_total = first_card + second_card
   display_card_total(card_total)
 end
