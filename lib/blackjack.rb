0c13ba29937538ed346b9bf4de091c48d9737e5c
Seohee  Im <seoheeim@vt.edu>
2020-08-10 15:59:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,7 +62,7 @@ def runner
   welcome
   current_total = initial_round
   card_total = current_total
-  while card_total >= 21  
+  until card_total >= 21  
     card_total = hit?(card_total)
     display_card_total(card_total)
   end 
